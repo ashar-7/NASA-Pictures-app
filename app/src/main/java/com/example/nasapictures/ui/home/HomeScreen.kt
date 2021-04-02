a3f2c7df1e659922eba14c247554c210f86ebc4a
ashar-7 <asharalikhan200@gmail.com>
2021-04-02 22:50:56 +0530

    Display Loading and Error states on Home Screen

--- app/src/main/java/com/example/nasapictures/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/nasapictures/ui/home/HomeScreen.kt
@@ -1,16 +1,23 @@
 package com.example.nasapictures.ui.home
 
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DarkMode
 import androidx.compose.material.icons.filled.LightMode
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.nasapictures.ImagesViewModel
 import com.example.nasapictures.R
 import com.example.nasapictures.UIState
@@ -49,16 +56,54 @@ fun HomeScreen(imagesViewModel: ImagesViewModel) {
             modifier = Modifier
                 .padding(padding)
         ) {
-            when (val uiState = imagesViewModel.uiState) {
-                is UIState.Idle -> {
+            Crossfade(imagesViewModel.uiState) { uiState ->
+                when (uiState) {
+                    is UIState.Idle -> {
+                    }
+                    is UIState.Loading -> {
+                        LoadingContent()
+                    }
+                    is UIState.Success -> {
+                        ImagesGrid(uiState.data)
+                    }
+                    is UIState.Failure -> {
+                        ErrorContent()
+                    }
                 }
-                is UIState.Loading -> {
-                }
-                is UIState.Success -> {
-                    ImagesGrid(uiState.data)
-                }
-                is UIState.Failure -> {
-                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun LoadingContent() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        CircularProgressIndicator(modifier = Modifier.size(42.dp))
+    }
+}
+
+@Composable
+private fun ErrorContent() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Column(
+            modifier = Modifier.padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Image(painterResource(R.drawable.robot_404), null)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.high) {
+                Text(
+                    stringResource(R.string.images_error_heading),
+                    style = MaterialTheme.typography.h6,
+                    textAlign = TextAlign.Center
+                )
+            }
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.high) {
+                Text(
+                    stringResource(R.string.images_error_description),
+                    style = MaterialTheme.typography.caption.copy(fontSize = 13.sp),
+                    textAlign = TextAlign.Center
+                )
             }
         }
     }
