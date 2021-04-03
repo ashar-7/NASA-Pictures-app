bc432ccb382ff0f9da564154c5312b3c94a0bf38
ashar-7 <asharalikhan200@gmail.com>
2021-04-03 20:17:32 +0530

    Change App Theme and remove Night Mode switch

--- app/src/main/java/com/example/nasapictures/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/nasapictures/ui/home/HomeScreen.kt
@@ -1,19 +1,15 @@
 package com.example.nasapictures.ui.home
 
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.animation.Crossfade
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.DarkMode
-import androidx.compose.material.icons.filled.LightMode
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import com.example.nasapictures.ImagesViewModel
-import com.example.nasapictures.R
 import com.example.nasapictures.UIState
 import com.example.nasapictures.data.NASAImage
 import com.example.nasapictures.ui.Screen
@@ -45,26 +41,8 @@ fun HomeScreen(imagesViewModel: ImagesViewModel, onImageSelected: (NASAImage) ->
 
 @Composable
 private fun HomeTopBar() {
-    val isDarkTheme = isSystemInDarkTheme()
     TopAppBar(
         title = { Text(Screen.Home.label) },
-        actions = {
-            IconButton(onClick = {
-                AppCompatDelegate.setDefaultNightMode(
-                    when {
-                        isDarkTheme -> AppCompatDelegate.MODE_NIGHT_NO
-                        else -> AppCompatDelegate.MODE_NIGHT_YES
-                    }
-                )
-            }) {
-                Icon(
-                    when {
-                        isDarkTheme -> Icons.Default.LightMode
-                        else -> Icons.Default.DarkMode
-                    },
-                    contentDescription = stringResource(R.string.toggle_night_mode)
-                )
-            }
-        }
+        backgroundColor = MaterialTheme.colors.primary
     )
 }
--- app/src/main/java/com/example/nasapictures/ui/theme/Color.kt
+++ app/src/main/java/com/example/nasapictures/ui/theme/Color.kt
@@ -2,7 +2,5 @@ package com.example.nasapictures.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val primaryDark = Color(0xFF121212)
+val white = Color.White
